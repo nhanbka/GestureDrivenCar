82ff25350d409d3b542be281ebfc76fcecd2e325
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2020-06-21 14:13:14 +0700

    Src

--- Src/Uno/DriveMotor.h
+++ Src/Uno/DriveMotor.h
@@ -1,8 +1,16 @@
 #include <AFMotor.h>
+#include "arm_controller.h"
+
+// DEFINE MODE OF CAR AND ARM
+#define CAR 0
+#define JAW 1
+#define HEIGHT 2
+#define RANGE 3
+
 #define Forward BACKWARD
 #define Backward FORWARD
 
-AF_DCMotor motorRight(2, MOTOR12_64KHZ);   // create motor #2, 64KHz pwm  
+AF_DCMotor motorRight(2, MOTOR12_64KHZ);   // create motor #2, 64KHz pwm
 AF_DCMotor motorLeft(1, MOTOR12_64KHZ);  // create motor #1, 64KHz pwm
 
 void motorSetup() {
@@ -10,38 +18,78 @@ void motorSetup() {
   motorRight.setSpeed(1000);
 }
 
-void goBackward() {
-  // running because of current signal
-  Serial.println("Go backward");     
-  motorLeft.run(Backward);     
-  motorRight.run(Backward);     
+void goBackward(int mode) {
+  if (mode == CAR) {
+    Serial.println("Go backward");
+    motorLeft.run(Backward);
+    motorRight.run(Backward);
+  } else {
+    if (mode == JAW) {
+      Serial.println("Release jaw");
+      jawRelease();
+    }
+    
+    if (mode == HEIGHT) {
+      Serial.println("Increase height");
+      heightIncrease();
+    }
+    
+    if (mode == RANGE) {
+      Serial.println("Decrease range");
+      rangeDecrease();
+    }
+  }
 }
 
-void turnLeft() {
-  // running because of current signal
-  Serial.println("Turning left");         
-  motorRight.run(Forward);     
-  motorLeft.run(Backward);  
+void turnLeft(int mode) {
+  if (mode == CAR) {
+    Serial.println("Turning left");
+    motorRight.run(Forward);
+    motorLeft.run(Backward);
+  } else {
+    Serial.println("Rotate left");
+    rotateLeft();
+  }
 }
 
-void turnRight() {
-  // running because of current signal
-  Serial.println("Turning right");   
-  motorRight.run(Backward);      
-  motorLeft.run(Forward);     
+void turnRight(int mode) {
+  if (mode == CAR) {
+    // running because of current signal
+    Serial.println("Turning right");
+    motorRight.run(Backward);
+    motorLeft.run(Forward);
+  } else {
+    Serial.println("Rotate right");
+    rotateRight();
+  }
 }
 
-void goForward() {
-  // running because of current signal
-  Serial.println("Go forward");     
-  motorLeft.run(Forward);     
-  motorRight.run(Forward);      
+void goForward(int mode) {
+  if (mode == CAR) {
+    Serial.println("Go forward");
+    motorLeft.run(Forward);
+    motorRight.run(Forward);
+  } else {
+    if (mode == JAW) {
+      Serial.println("Bind jaw");
+      jawBind();
+    }
+
+    if (mode == HEIGHT) {
+      Serial.println("Decrease height");
+      heightDecrease();
+    }
+
+    if (mode == RANGE) {
+      Serial.println("Increase range");
+      rangeIncrease();
+    }
+  }
 }
 
 
 void stopCar() {
-  // running because of current signal
- Serial.println("Stop");     
- motorLeft.run(RELEASE);     
- motorRight.run(RELEASE);     
+  Serial.println("Stop");
+  motorLeft.run(RELEASE);
+  motorRight.run(RELEASE);
 }
--- Src/Uno/Uno.ino
+++ Src/Uno/Uno.ino
@@ -1,47 +1,46 @@
 #include <SoftwareSerial.h>
 #include "DriveMotor.h"
 
-
 #define GOING_FORWARD 'F'
 #define GOING_BACKWARD 'B'
 #define TURNING_LEFT 'L'
 #define TURNING_RIGHT 'R'
 #define STOP 'S'
 
+SoftwareSerial BTserial(2, 3); // RX | TX
 
-SoftwareSerial BTserial(2, 3); // RX | TX 
-
-
-char d = 0; 
+char d = 0;
+int mode = 0;
 
 void setup() {
   Serial.begin(38400);
+  initState();  
   Serial.println("Arduino is ready");
   // HC-05 default serial speed for AT mode is 38400
   BTserial.begin(38400);
 
   motorSetup();
+  mode = 0;
 }
 
-
 void loop() {
-  if(BTserial.available() > 0){
+  if (BTserial.available() > 0) {
     d = BTserial.read();
-    Serial.write(d);
-    if(d == GOING_FORWARD){
-      goForward();
-    }
-    if(d == TURNING_RIGHT){
-      turnRight();
-    }
-    if(d == GOING_BACKWARD){
-      goBackward();
-    }
-     if(d == TURNING_LEFT){
-      turnLeft();
-    }
-    if(d == STOP){
+    Serial.println(mode);
+    Serial.println(d);
+    if (d >= '0' && d < '4') {
+      mode = (int)(d - '0');
+    } else if (d == GOING_FORWARD) {
+      goForward(mode);
+    } else if (d == TURNING_RIGHT) {
+      turnRight(mode);
+    } else if (d == GOING_BACKWARD){
+      goBackward(mode);
+    } else if (d == TURNING_LEFT) {
+      turnLeft(mode);
+    } else if (d == STOP) {
       stopCar();
     }
+    Serial.println(mode);
   }
 }
